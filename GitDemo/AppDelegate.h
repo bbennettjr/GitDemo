20d8d6a6542db846b97efc33aca1be72fc442d6f
Brad Bennett <bbennettjr@gmail.com>
2016-02-20 14:12:15 -0500

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Bradford Bennett Jr on 2/20/16.
+//  Copyright © 2016 Bradford Bennett Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Bradford Bennett Jr on 2/20/16.
+//  Copyright © 2016 Bradford Bennett Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
